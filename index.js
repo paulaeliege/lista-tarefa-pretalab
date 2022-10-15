5d5b44798e156ba79c1ff76b80bbf4e1c5dfa6b4
Paula <paulaeliegeferreira@gmail.com>
2022-10-15 11:35:55 -0300

    lista-tarefa-pretalab

---
+++ index.js
@@ -0,0 +1,15 @@
+let id = 0
+
+const tarefa = (id, novaTarefa) => `<div id='${id}'>
+<p>${novaTarefa}</p>
+<input type="checkbox" />
+</div>`
+
+
+function adicionarTarefa(){ 
+    id++
+    const atividade = document.getElementById('nome-tarefa').value
+    
+    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, atividade)
+    
+}
