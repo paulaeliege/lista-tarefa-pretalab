dab1a474fadae236f12cf060f6c699c653f9a313
Paula <paulaeliegeferreira@gmail.com>
2022-10-22 11:43:35 -0300

    Adicionando LocalStorage

--- index.js
+++ index.js
@@ -6,10 +6,32 @@ const tarefa = (id, novaTarefa) => `<div id='${id}'>
 </div>`
 
 
-function adicionarTarefa(){ 
+function exibirLista() {
     id++
-    const atividade = document.getElementById('nome-tarefa').value
-    
-    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, atividade)
-    
+    const tarefas = JSON.parse(localStorage.getItem('lista-tarefas'))
+    if (tarefas) {
+        tarefas.forEach(tarefaListada => {
+            document.querySelector('#lista-tarefas').innerHTML += tarefa(id, tarefaListada)
+
+        })
+    }
 }
+
+
+function adicionarTarefa() {
+    id++
+    const novaTarefa = document.getElementById('nome-tarefa').value
+    document.querySelector('#lista-tarefas').innerHTML += tarefa(id, novaTarefa)
+    const listaTarefas = localStorage.getItem('lista-tarefas')
+    if (listaTarefas) {
+        const novaLista = JSON.parse(listaTarefas)
+        novaLista.push(novaTarefa)
+        localStorage.setItem('lista-tarefas', JSON.stringify(novaLista))
+    } else {
+
+        localStorage.setItem('lista-tarefas', JSON.stringify([novaTarefa]))
+    }
+
+}
+
+exibirLista()
